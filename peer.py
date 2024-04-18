0cf95e5e9b7d7ed95cc63612814e07df6cea8fbf
Germanyy0410 <tienduchoang136@gmail.com>
2024-04-18 20:04:25 +0700

    feat: adding IP

--- peer.py
+++ peer.py
@@ -1,14 +1,27 @@
 import requests # type: ignore
 import os
 from dotenv import load_dotenv # type: ignore
+from datetime import datetime
+import socket
 
 load_dotenv()
+
+def get_time():
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%d/%m %H:%M:%S")
+    return formatted_time
+
+def get_ip():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return ip
 
 def connect_to_tracker():
     torrent_info = {
         "info_hash": "dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c",
-        "peer_id": "your_ubuntu_peer_id",
+        "peer_id": "Ubuntu " + get_time(),
         "port": 54321,  # Port của peer trên Ubuntu
+        "ip": get_ip(),
         "event": "started"
     }
     response = requests.get("http://" + os.environ['CURRENT_IP'] + ":8080/announce", params=torrent_info)
