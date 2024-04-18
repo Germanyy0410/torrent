29712170bce1fbf28ddabe1c344005906bb42cd7
Germanyy0410 <tienduchoang136@gmail.com>
2024-04-18 19:31:23 +0700

    feat: update peer...

--- peer.py
+++ peer.py
@@ -1,17 +1,20 @@
-import requests # type: ignore
-import json
+import requests
+import os
+from dotenv import load_dotenv # type: ignore
+
+load_dotenv()
 
 def connect_to_tracker():
     torrent_info = {
         "info_hash": "dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c",
-        "peer_id": "your_windows_peer_id",
-        "port": 12345,  # Port của peer trên Windows
+        "peer_id": "your_ubuntu_peer_id",
+        "port": 54321,  # Port của peer trên Ubuntu
         "event": "started"
     }
-    response = requests.get("http://192.168.0.6:8080", params=torrent_info)
+    response = requests.get("http://" + os.environ['CURRENT_IP'] + ":8080/announce", params=torrent_info)
     return response.json()
 
 if __name__ == "__main__":
     tracker_response = connect_to_tracker()
-    print("Tracker response:", json.dumps(tracker_response, indent=4))
+    print("Tracker response:", tracker_response)
     # Tiếp tục xử lý response để thiết lập kết nối với các peer khác và truyền tải dữ liệu
