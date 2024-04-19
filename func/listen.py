46a519be18bd93faec360e45184a7bd173e58993
Germanyy0410 <tienduchoang136@gmail.com>
2024-04-19 19:37:33 +0700

    fix: download file from peers successfully

--- func/listen.py
+++ func/listen.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # Tạo socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,11 +18,21 @@ while True:
     # Chấp nhận kết nối từ máy khách
     client_socket, client_address = server_socket.accept()
 
-    # Nhận dữ liệu từ máy khách
-    received_data = client_socket.recv(1024).decode()
-    print("Received data:", received_data)
+    # Nhận đường dẫn của file từ máy khách
+    file_path = client_socket.recv(1024).decode()
+    print("Received file path:", file_path)
 
-    # Đóng kết nối
+    # Kiểm tra sự tồn tại của file
+    if os.path.exists(file_path):
+        # Mở file và gửi dữ liệu cho máy khách
+        with open(file_path, 'rb') as file:
+            data = file.read()
+            client_socket.sendall(data)
+            print("File '{}' đã được gửi thành công.".format(file_path))
+    else:
+        print("File '{}' không tồn tại.".format(file_path))
+
+    # Đóng kết nối với máy khách
     client_socket.close()
 
 # Đóng socket chính
--- test.py
+++ test.py
@@ -1,58 +1,30 @@
 import socket
-import os
-import time
 
-class FilePart:
-    def __init__(self, file_number, existed):
-        self.file_number = file_number
-        self.existed = existed
+# Tạo socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def get_file_part_status(folder_path, file_name):
-    file_parts = []
+# Địa chỉ IP và số cổng của máy ảo Ubuntu
+host = '192.168.227.130'  # Địa chỉ IP của máy ảo Ubuntu
+port = 1234  # Số cổng mà server đang lắng nghe
 
-    for i in range(1, 5):
-        file_part = str(i)  + '_' + file_name + '.part'
-        file_path = os.path.join(folder_path, file_part)
-        file_parts.append(FilePart(i, os.path.exists(file_path)))
-    return file_parts
+# Kết nối đến máy chủ trên máy ảo Ubuntu
+client_socket.connect((host, port))
 
-def download_file(peer_ip, peer_port, server_folder, file_parts):
-    # Create a TCP socket
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Nhập đường dẫn của file trên máy ảo Ubuntu
+file_path = '/home/germanyy0410/logbuf.c'
 
-    try:
-        # Connect to the peer
-        client_socket.connect((peer_ip, peer_port))
+# Gửi yêu cầu tải file
+client_socket.send(file_path.encode())
 
-        # Iterate through file parts
-        for part in file_parts:
-            if not part.existed:  # Only download parts that don't exist locally
-                # Send request for the file part
-                client_socket.send(str(part.file_number).encode())
+# Nhận dữ liệu từ máy chủ (dữ liệu của file)
+with open('downloaded_file.txt', 'wb') as file:
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            print("File đã được tải về thành công.")
+            break
+        file.write(data)
 
-                # Receive file data
-                file_path = os.path.join('D:/CN_Ass/input/book/', f'{part.file_number}.part')
-                with open(file_path, 'wb') as file:
-                    while True:
-                        data = client_socket.recv(1024)
-                        if not data:
-                            break
-                        file.write(data)
 
-                print(f"Part {part.file_number} downloaded successfully.")
-
-    except Exception as e:
-        print("Error:", e)
-
-    finally:
-        # Close the socket
-        client_socket.close()
-
-# Usage example
-peer_ip = '192.168.227.130'
-peer_port = 1234
-server_folder = '/home/germanyy0410/Desktop/cn/torrent/input/excel'
-
-file_parts = get_file_part_status('D:/CN_Ass/input/excel', 'excel')
-
-download_file(peer_ip, peer_port, server_folder, file_parts)
+# Đóng kết nối
+client_socket.close()
