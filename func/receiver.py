a3ff538de21410ccae133bf3fe4148be19ed0ae5
Germanyy0410 <tienduchoang136@gmail.com>
2024-04-21 11:31:18 +0700

    fix: minor changes

--- func/receiver.py
+++ func/receiver.py
@@ -1,5 +1,6 @@
 import socket
 
+# host = '192.168.227.130'
 host = '192.168.1.8'
 port = 1234
 
@@ -8,6 +9,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
 receive_path = '/home/germanyy0410/cn/torrent/output'
+# receive_path = 'D:/CN_Ass/func/res.mp4'
 
 try:
   with open(receive_path, 'wb') as file:
--- func/sender.py
+++ func/sender.py
@@ -1,6 +1,9 @@
 import socket
+import os
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# host = '192.168.227.130'
 host = '192.168.1.8'
 port = 1234
 
@@ -8,17 +11,23 @@ server_socket.bind((host, port))
 
 server_socket.listen(1)
 
-while True:
-  client_socket, client_address = server_socket.accept()
+file_path = 'D:/CN_Ass/func/Inside Part 1.mp4'
+# file_path = '/home/germanyy0410/cn/torrent/func/video.mp4'
 
-  file_path = 'D:/CN_Ass/func/Inside Part 1.mp4'
+file_size = os.path.getsize(file_path)
 
-  with open(file_path, 'rb') as file:
+with open(file_path, 'rb') as file:
     data = file.read()
-    try:
-      client_socket.sendall(data)
-      print("Send successfully")
-    except BrokenPipeError:
-      pass
 
-  client_socket.close()
+server_socket.listen(1)
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    client_socket.sendall(str(file_size).encode())  # Send file size first
+    try:
+        client_socket.sendall(data)
+        print("Send successfully")
+    except BrokenPipeError:
+        pass
+
+    client_socket.close()
