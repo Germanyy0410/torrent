222f989213898e97dc1e25388680730296835762
Germanyy0410 <tienduchoang136@gmail.com>
2024-05-02 14:00:25 +0700

    feat: update UI

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from prettytable import PrettyTable
 
 class colors:
     RED = '\033[91m'
+    RED_BOLD = '\033[91m\033[1m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     BLUE = '\033[94m'
@@ -20,11 +21,11 @@ class colors:
     RESET = '\033[0m'
 
 def get_input_path(file_name):
-    return (os.path.dirname(os.path.realpath(__file__)) + '/input/' + file_name).replace('\\', '/')
+    return (os.getcwd() + '/input/' + file_name).replace('\\', '/')
 
 
 def get_output_path(file_name):
-    return (os.path.dirname(os.path.realpath(__file__)) + '/output/' + file_name).replace('\\', '/')
+    return (os.getcwd() + '/output/' + file_name).replace('\\', '/')
 
 
 def get_torrent_path(file_name):
@@ -34,26 +35,19 @@ def get_torrent_path(file_name):
 #* ================================ TORRENT ===============================
 
 def create_torrent(directory_path, tracker_url, piece_length=512 * 1024):
-    # Khởi tạo danh sách các file
     files = []
 
-    # Lặp qua tất cả các file trong thư mục
     for root, _, filenames in os.walk(directory_path):
         for filename in filenames:
-            # Đường dẫn tuyệt đối của file
             file_path = os.path.join(root, filename)
 
-            # Đọc nội dung của file
             with open(file_path, 'rb') as f:
                 file_data = f.read()
 
-            # Tính toán độ dài của file
             file_size = len(file_data)
 
-            # Tính toán số lượng phần
             num_pieces = -(-file_size // piece_length)
 
-            # Tính toán hash và kích thước của từng phần và sắp xếp chúng thành một danh sách
             pieces = []
             for i in range(0, file_size, piece_length):
                 piece_data = file_data[i:i+piece_length]
@@ -63,7 +57,6 @@ def create_torrent(directory_path, tracker_url, piece_length=512 * 1024):
 
             file_hash = hashlib.sha1(file_data).digest()
 
-            # Thêm thông tin của file vào danh sách
             file_info = {
                 'path': file_path.replace("\\", "/"),
                 'name': file_path.replace("\\", "/").split('/')[-1],
@@ -74,7 +67,6 @@ def create_torrent(directory_path, tracker_url, piece_length=512 * 1024):
             }
             files.append(file_info)
 
-    # Tạo thông tin của torrent
     torrent_info = {
         'info': {
             'name': directory_path,
@@ -86,15 +78,13 @@ def create_torrent(directory_path, tracker_url, piece_length=512 * 1024):
 
     info_hash = hashlib.sha1(bencode(torrent_info['info'])).digest()
     torrent_info['info']['info_hash'] = info_hash
-    # Mã hóa thông tin của torrent bằng Bencoding
     torrent_data = bencode(torrent_info)
 
-    # Lưu dữ liệu của torrent vào file
     torrent_file_path = os.path.join(directory_path, directory_path.split('/')[-1] + '.torrent')
     with open(torrent_file_path, 'wb') as torrent_file:
         torrent_file.write(torrent_data)
 
-    print("\n{} is successfully created.\n".format(directory_path))
+    print("\n{} is successfully created.".format(torrent_file_path))
 
 
 def read_torrent(torrent_file_path):
@@ -176,15 +166,9 @@ def get_peers_from_tracker():
 
 #* ========================================================================
 
-running = True
-
-
 def torrent_start(torrent_name):
     peers = get_peers_from_tracker()
-    print(peers)
-    # torrent_name = input("Please input file name you want to download: ")
     input = get_torrent_status(torrent_name)
-    inputs = json.dumps(input.to_dict())
 
     threads = []
     # Connect client to peer(s)
@@ -197,64 +181,135 @@ def torrent_start(torrent_name):
         thread.join()
 
 
-def torrent_show(torrent_name):
+def torrent_info(torrent_name):
     torrent_info, piece_hashes = read_torrent(get_torrent_path(torrent_name))
-    print("\nannounce URL: {}".format(torrent_info["announce"]))
-    print("info hash: {}\n".format(torrent_info["info_hash"].hex()))
+    print("\n•  announce URL: {}".format(torrent_info["announce"]))
+    print("•  info hash: {}\n".format(torrent_info["info_hash"].hex()))
 
-    table = PrettyTable(["file", "size", "piece count", "file hash"])
+    table = PrettyTable([colors.YELLOW + "file" + colors.RESET, colors.YELLOW + "size" + colors.RESET, colors.YELLOW + "no. pieces" + colors.RESET, colors.YELLOW + "file hash" + colors.RESET])
     for file in torrent_info["files"]:
         table.add_row([file["name"], get_piece_size(file["length"]), file["num_pieces"], file["file_hash"].hex()])
-    print(table.get_string() + '\n')
+    print(table.get_string())
 
-# def user_input_thread():
-#     global running
-#     input()
-#     running = False
-#     user_command = input("Nhập câu lệnh: ")
-#     if user_command == "b-create":
-#         # TODO:
-#         print("a")
-#     elif user_command == "b-start":
-#         # TODO:
-#         print("a")
-#     elif user_command == "b-show":
-#         # TODO:
-#         print("a")
-#     elif user_command == "b-info":
-#         # TODO:
-#         print("a")
 
-# input_thread = threading.Thread(target=user_input_thread)
-# input_thread.start()
+def torrent_show():
+    folder_path = os.getcwd() + '/input/'
+    torrent_files = []
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".torrent"):
+                torrent_files.append(os.path.join(root, file))
 
+    print("\nList of torrent files:")
+    for file in torrent_files:
+        print("   |-- {}".format(file.replace("\\", "/")))
+
+    return torrent_files
+
+
+def torrent_edit_tracker_url(torrent_name, new_tracker_url):
+    torrent_file_path = get_torrent_path(torrent_name)
+    with open(torrent_file_path, 'rb') as torrent_file:
+        torrent_data = torrent_file.read()
+        decoded_data = bdecode(torrent_data)
+
+    if 'announce' in decoded_data:
+        decoded_data['announce'] = new_tracker_url
+
+        with open(torrent_file_path, 'wb') as torrent_file:
+            torrent_file.write(bencode(decoded_data))
+        print("Change tracker URL successfully to " + new_tracker_url)
+    else:
+        print("Torrent file does not contain tracker URL")
 
 #* ========================== START APPLICATION ===========================
+
+input_command = True
+
+def user_input_thread():
+    global input_command
+    while True:
+        user_command = input(colors.GREEN + "\n>> Enter command here: " + colors.RESET)
+
+        if "b-create" in user_command:
+            if len(user_command.split(" ")) != 3:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                dir_path = user_command.split(" ")[1]
+                tracker_url = user_command.split(" ")[2]
+                if os.path.exists(dir_path):
+                    create_torrent(dir_path, tracker_url)
+                else:
+                    print("Directory not found, please try again")
+
+        elif "b-show" in user_command:
+            if len(user_command.split(" ")) != 1:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                torrent_show()
+
+        elif "b-info" in user_command:
+            if len(user_command.split(" ")) != 2:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                torrent_name = user_command.split(" ")[1]
+                torrent_info(torrent_name)
+
+        elif "b-start" in user_command:
+            if len(user_command.split(" ")) != 2:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                torrent_name = user_command.split(" ")[1]
+                torrent_start(torrent_name)
+
+        elif "b-help" in user_command:
+            if len(user_command.split(" ")) != 1:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                print_input_help()
+
+        elif "b-edit" in user_command:
+            if (len(user_command.split(" ")) != 3):
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                torrent_name = user_command.split(" ")[1]
+                new_tracker_url = user_command.split(" ")[2]
+                torrent_edit_tracker_url(torrent_name, new_tracker_url)
+
+        elif "b-close" in user_command:
+            if len(user_command.split(" ")) != 1:
+                print (colors.RED_BOLD + "\nSyntax Error: " + colors.RESET + "Command not found")
+            else:
+                print(colors.YELLOW + "\nWarning: " + colors.RESET +"Closing application...\n")
+                break
+
+        else:
+            print (colors.RED_BOLD + "\nError: " + colors.RESET + "Command not found")
+
+
+def print_input_help():
+    print("\nSome useful commands you should use:\n")
+    print("•  " + colors.BLUE + "b-create <path> <tracker-url>:" + colors.RESET + " Create a .torrent file for the content at the specified path, with the given tracker URL.\n")
+    print("•  " + colors.BLUE + "b-show:" + colors.RESET + " Display all managed torrent files, helping you keep track of your torrents easily.\n")
+    print("•  " + colors.BLUE + "b-info <torrent-file>:" + colors.RESET + " View metadata associated with a .torrent file, such as file names, sizes, and hash values.\n")
+    print("•  " + colors.BLUE + "b-start <torrent-file>:" + colors.RESET + " Start downloading files from the specified .torrent file seamlessly.\n")
+    print("•  " + colors.BLUE + "b-edit <torrent-file> <new-tracker-url>:" + colors.RESET + " Modify the announce URL of a .torrent file, providing flexibility in managing torrents efficiently.\n")
+    print("•  " + colors.BLUE + "b-help:" + colors.RESET + " View all commands and usages.\n")
+    print("•  " + colors.BLUE + "b-close:" + colors.RESET + " Close the application.")
+
+
 if __name__ == '__main__':
-    # text_based = """
-    #  ___  ___  _____   _____  ___   ___  ___  _  _  _____
-    # | _ )|_ _||_   _| |_   _|/ _ \ | _ \| __|| \| ||_   _|
-    # | _ \ | |   | |     | | | (_) ||   /| _| | .` |  | |
-    # |___/|___|  |_|     |_|  \___/ |_|_\|___||_|\_|  |_|
-    # """
-    # print(text_based)
+    text_based = """
+                     ___  ___  _____   _____  ___   ___  ___  _  _  _____
+                    | _ )|_ _||_   _| |_   _|/ _ \ | _ \| __|| \| ||_   _|
+                    | _ \ | |   | |     | | | (_) ||   /| _| | .` |  | |
+                    |___/|___|  |_|     |_|  \___/ |_|_\|___||_|\_|  |_|
+    """
+    os.system("cls")
+    print(colors.MAGENTA + text_based + colors.RESET)
 
-    # input(colors.GREEN + ">> Enter command hehe: " + colors.RESET)
-
-    # print("\nList of torrent files:")
-    # print("   |-- books.torrent")
-    # print("   |-- videos.torrent")
-    # print("   |-- slides.torrent")
-
-    # input(colors.GREEN + "\n>> Enter command hehe: " + colors.RESET)
-
-
-    # print("\nopening books.torrent...")
-
-    # torrent_show('books')
-    # create_torrent("D:/CN_Ass/output/books", 'http:/10.46.153.20:8080/announce')
-     # Calculate elapsed time
-    torrent_name = 'books'
-    torrent_start(torrent_name)
+    print_input_help()
+    input_thread = threading.Thread(target=user_input_thread)
+    input_thread.start()
 
 #* ========================================================================
